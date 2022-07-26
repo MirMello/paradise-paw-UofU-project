b61fe2668e9b69ce43b04fe786dbe438fe0878c7
MirMello <mirandaleeallen@gmail.com>
2022-07-26 16:22:36 -0600

    changed main.handlebars to only display the header and footer

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+
+
 const PORT = process.env.PORT || 3002;
